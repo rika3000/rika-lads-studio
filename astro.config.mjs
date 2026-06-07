ce3a1e7f7fd009b3421a0429faad1fa5e91f63a5
rika3000 <rika.rika3000.rika@gmail.com>
2026-06-07 13:53:20 -0600

    removed unused library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { site } from 'astro:config/server';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://rika3000.github.io",
+    base: "/rika-lads-studio",
     devToolbar: {
         enabled: false,
     },
     redirects: {
         "/": "/about"
     },
-    site: "https://rika3000.github.io",
-    base: "/rika-lads-studio",
 });
